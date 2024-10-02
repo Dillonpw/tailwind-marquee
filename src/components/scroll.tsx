ff811f91330a4eca964022427b0c37100e4030e8
dillonpw <dillonpwalsh10@gmail.com>
2024-10-02 16:56:02 -0400

    minimal style exmaple

--- src/components/scroll.tsx
+++ src/components/scroll.tsx
@@ -1,11 +1,11 @@
 const Scroll = () => {
   return (
-    <div className="relative flex w-full flex-col justify-center overflow-hidden">
-      <div className="pointer-events-none relative flex overflow-hidden">
+    <div className="flex min-w-full flex-col justify-center overflow-hidden">
+      <div className="flex overflow-hidden">
         <div className="flex min-w-full animate-marquee items-center justify-around">
           {["Text 1", "Text 2", "Text 3", "Text 4", "Text 5"].map(
             (text, index) => (
-              <div key={index} className="p-4">
+              <div key={index}>
                 <p>{text}</p>
               </div>
             ),
@@ -14,7 +14,7 @@ const Scroll = () => {
         <div className="flex min-w-full animate-marquee items-center justify-around">
           {["Text 1", "Text 2", "Text 3", "Text 4", "Text 5"].map(
             (text, index) => (
-              <div key={index} className="p-4">
+              <div key={index}>
                 <p>{text}</p>
               </div>
             ),
