50f11e639ac92f80dd42482e884f807f1bfcf58c
dillonpw <dillonpwalsh10@gmail.com>
2024-10-02 17:26:17 -0400

    fomratting

--- src/components/scroll.tsx
+++ src/components/scroll.tsx
@@ -1,24 +1,42 @@
 const Scroll = () => {
   return (
-    <div className="flex min-w-full flex-col justify-center overflow-hidden">
+    <div className="flex min-w-full justify-center overflow-hidden">
       <div className="flex overflow-hidden">
         <div className="flex min-w-full animate-marquee items-center justify-around">
-          {["Lorem", "Ipsum", "Dolor", "Sit", "Amet", "Consectetur", "Adipiscing", "Elit", "Sed", "Do"].map(
-            (text, index) => (
-              <div key={index}>
-                <p>{text}</p>
-              </div>
-            ),
-          )}
+          {[
+            "Lorem",
+            "Ipsum",
+            "Dolor",
+            "Sit",
+            "Amet",
+            "Consectetur",
+            "Adipiscing",
+            "Elit",
+            "Sed",
+            "Do",
+          ].map((text, index) => (
+            <div key={index}>
+              <p>{text}</p>
+            </div>
+          ))}
         </div>
         <div className="flex min-w-full animate-marquee items-center justify-around">
-        {["Lorem", "Ipsum", "Dolor", "Sit", "Amet", "Consectetur", "Adipiscing", "Elit", "Sed", "Do"].map(
-            (text, index) => (
-              <div key={index}>
-                <p>{text}</p>
-              </div>
-            ),
-          )}
+          {[
+            "Lorem",
+            "Ipsum",
+            "Dolor",
+            "Sit",
+            "Amet",
+            "Consectetur",
+            "Adipiscing",
+            "Elit",
+            "Sed",
+            "Do",
+          ].map((text, index) => (
+            <div key={index}>
+              <p>{text}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
