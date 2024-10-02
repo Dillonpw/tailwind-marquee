a19c744a2a8c3e5db3d83b8c3f4143fc322a28c2
dillonpw <dillonpwalsh10@gmail.com>
2024-10-02 16:30:47 -0400

    minimizing unused styles

--- src/components/scroll.tsx
+++ src/components/scroll.tsx
@@ -1,8 +1,8 @@
 const Scroll = () => {
   return (
-    <div className="relative flex w-full flex-col justify-center overflow-hidden border-b-2">
-      <div className="pointer-events-none relative flex gap-0 overflow-hidden">
-        <div className="flex min-w-full shrink-0 animate-marquee items-center justify-around gap-0">
+    <div className="relative flex w-full flex-col justify-center overflow-hidden">
+      <div className="pointer-events-none relative flex overflow-hidden">
+        <div className="flex min-w-full animate-marquee items-center justify-around">
           {["Text 1", "Text 2", "Text 3", "Text 4", "Text 5"].map(
             (text, index) => (
               <div key={index} className="p-4">
@@ -11,7 +11,7 @@ const Scroll = () => {
             ),
           )}
         </div>
-        <div className="flex min-w-full shrink-0 animate-marquee items-center justify-around gap-0">
+        <div className="flex min-w-full animate-marquee items-center justify-around">
           {["Text 1", "Text 2", "Text 3", "Text 4", "Text 5"].map(
             (text, index) => (
               <div key={index} className="p-4">
