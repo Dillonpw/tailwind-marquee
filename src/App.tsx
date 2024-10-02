0d2c6ab79ccf17642b57015d2a5503473f9760a9
dillonpw <dillonpwalsh10@gmail.com>
2024-10-02 15:05:39 -0400

    adjusting example styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Scroll from "./components/scroll";
 
 function App() {
   return (
-    <main>
+    <main className="flex items-center justify-center">
       <Scroll />
     </main>
   );
--- src/components/scroll.tsx
+++ src/components/scroll.tsx
@@ -1,6 +1,6 @@
 const Scroll = () => {
   return (
-    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+    <div className="relative flex w-full flex-col justify-center overflow-hidden border-b-2">
       <div className="pointer-events-none relative flex gap-0 overflow-hidden">
         <div className="flex min-w-full shrink-0 animate-marquee items-center justify-around gap-0">
           {["Text 1", "Text 2", "Text 3", "Text 4", "Text 5"].map(
@@ -11,10 +11,7 @@ const Scroll = () => {
             ),
           )}
         </div>
-        <div
-          aria-hidden="true"
-          className="flex min-w-full shrink-0 animate-marquee items-center justify-around gap-0"
-        >
+        <div className="flex min-w-full shrink-0 animate-marquee items-center justify-around gap-0">
           {["Text 1", "Text 2", "Text 3", "Text 4", "Text 5"].map(
             (text, index) => (
               <div key={index} className="p-4">
